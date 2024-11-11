4705cbb05974b21015eb01633914d736bca9eba5
George-25 <georgemallory26@gmail.com>
2024-11-11 18:39:42 -0500

    exercise-6

--- functions.py
+++ functions.py
@@ -111,6 +111,21 @@ def sum_to(n):
 def largest(num1,num2,num3):
     return max(num1,num2,num3);
 
-print('Exercise 6:', largest(1, 2, 3));
-print('Exercise 6:', largest(10, 4, 2));
+# print('Exercise 6:', largest(1, 2, 3));
+# print('Exercise 6:', largest(10, 4, 2));
 
+# Exercise 7: Calculate a Tip
+#
+# Create a function called `calculate_tip`. It should take the bill amount and the tip percentage (as a whole number).
+# The function should return the amount of the tip.
+#
+# Examples:
+# calculate_tip(50, 20) should return 10.
+#
+# Write your function and test its output below.
+
+def calculate_tip(bill,tip):
+    tip = bill * (tip / 100);
+    return tip;
+
+print('Exercise 7:', calculate_tip(50, 20))
